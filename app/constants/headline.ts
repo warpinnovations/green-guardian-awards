a0f0de1e64e1804f56c27d93b4ff076ac36de396
karmslajo <karms.lajo@gmail.com>
2026-03-24 16:26:06 +0800

    feat: update image URLs for historical headlines to use relative paths

--- app/constants/headline.ts
+++ app/constants/headline.ts
@@ -11,153 +11,153 @@ export const headlines: Headline[] = [
     "date": "July 12, 2002",
     "topic": "The Killing of Espinosa",
     "headline": "Espinosa killed in Zamora Shooting: A member of Espinosa family was killed by a lone gunman remains at large.",
-    "imageUrl": "https://example.com/images/espinosa-2002.jpg"
+    "imageUrl": "/dg-headlines/2002.jpg"
   },
   {
     "date": "March 6, 2003",
     "topic": "Bomb Explosion",
     "headline": "ALERT Iloilo braces vs terrorism: Iloilo’s officialdom tolled alarm bells all over the city and province to brace for the possible spread of terrorism into this part of country following the bombing outside Davao airport.",
-    "imageUrl": "https://example.com/images/iloilo-terror-alert-2003.jpg"
+    "imageUrl": "/dg-headlines/2003.jpg"
   },
   {
     "date": "September 10, 2004",
     "topic": "RPA-ABB’s Leaders “Ka Mokong’s” Death",
     "headline": "NPA CLAIMS MOKONG’S SLAY, ANTIQUE AMBUSH: Communist rebels in Panay claimed responsibility for the killing of RPA-ABB leader Daniel “Ka Mokong” Batoy.",
-    "imageUrl": "https://example.com/images/mokong-2004.jpg"
+    "imageUrl": "/dg-headlines/2004.jpg"
   },
   {
     "date": "November 21, 2005",
     "topic": "NPA Landmines killed 9 Army soldiers",
     "headline": "AMBUSH 9 KILLED, 14 WOUNDED IN CALINOG: The Philippine Army denounced the NPA for using landmines.",
-    "imageUrl": "https://example.com/images/calinog-ambush-2005.jpg"
+    "imageUrl": "/dg-headlines/2005.jpg"
   },
   {
     "date": "January 13, 2006",
     "topic": "Bobby Tan Murder Case",
     "headline": "‘TAN SONS DID IT’: CIDG files murder raps vs suspects tied to business motive.",
-    "imageUrl": "https://example.com/images/bobby-tan-2006.jpg"
+    "imageUrl": "/dg-headlines/2006.jpg"
   },
   {
     "date": "January 18, 2007",
     "topic": "Capitol Siege",
     "headline": "CA STOPS CAPITOL SIEGE: Armed policemen stormed the Capitol as a TRO allowed Gov. Tupas to stay.",
-    "imageUrl": "https://example.com/images/capitol-siege-2007.jpg",
+    "imageUrl": "/dg-headlines/2007.jpg",
     "link": "https://www.facebook.com/share/p/18RepsrPii/"
   },
   {
     "date": "June 23-24, 2008",
     "topic": "Typhoon Frank",
     "headline": "“FRANK” DEATH TOLL IN WV REACHES 123.",
-    "imageUrl": "https://example.com/images/typhoon-frank-2008.jpg"
+    "imageUrl": "/dg-headlines/2008.jpg"
   },
   {
     "date": "November 5, 2009",
     "topic": "Gonzales–Treñas Split",
     "headline": "JR. TO JERRY: ‘WHY CAN’T YOU WAIT?’",
-    "imageUrl": "https://example.com/images/gonzales-trenas-2009.jpg"
+    "imageUrl": "/dg-headlines/2009.jpg"
   },
   {
     "date": "May 15-16, 2010",
     "topic": "Elections",
     "headline": "TREÑAS–MABILOG ENDS REIGN OF RAUL GONZALEZ.",
-    "imageUrl": "https://example.com/images/elections-2010.jpg"
+    "imageUrl": "/dg-headlines/2010.jpg"
   },
   {
     "date": "April 2-3, 2011",
     "topic": "Coal Plant",
     "headline": "DAWN OF PANAY’S ECONOMIC POWER.",
-    "imageUrl": "https://example.com/images/coal-plant-2011.jpg"
+    "imageUrl": "/dg-headlines/2011.jpg"
   },
   {
     "date": "December 3, 2012",
     "topic": "Typhoon Pablo",
     "headline": "ILOILO GEARING FOR SUPER TYPHOON PABLO.",
-    "imageUrl": "https://example.com/images/pablo-2012.jpg"
+    "imageUrl": "/dg-headlines/2012.jpg"
   },
   {
     "date": "November 11, 2013",
     "topic": "Typhoon Yolanda",
     "headline": "TRAIL OF DEATH & DESTRUCTION.",
-    "imageUrl": "https://example.com/images/yolanda-2013.jpg",
+    "imageUrl": "/dg-headlines/2013.jpg",
     "link": "https://www.facebook.com/photo.php?fbid=689070457779589"
   },
   {
     "date": "March 17, 2014",
     "topic": "Capalla Murder",
     "headline": "Who murdered Romy Capalla?",
-    "imageUrl": "https://example.com/images/capalla-2014.jpg"
+    "imageUrl": "/dg-headlines/2014.jpg"
   },
   {
     "date": "November 3, 2015",
     "topic": "Tanim Bala",
     "headline": "Iloilo airport safe vs ‘tanim-bala’ racket?",
-    "imageUrl": "https://example.com/images/tanim-bala-2015.jpg"
+    "imageUrl": "/dg-headlines/2015.jpg"
   },
   {
     "date": "July 8, 2016",
     "topic": "War on Drugs",
     "headline": "941 ‘drug pushers, users’ surrender.",
-    "imageUrl": "https://example.com/images/tokhang-2016.jpg"
+    "imageUrl": "/dg-headlines/2016.jpg"
   },
   {
     "date": "October 19, 2017",
     "topic": "Airsoft Gang",
     "headline": "‘Airsoft’ gang strikes in city.",
-    "imageUrl": "https://example.com/images/airsoft-2017.jpg"
+    "imageUrl": "/dg-headlines/2017.jpg"
   },
   {
     "date": "April 6, 2018",
     "topic": "Boracay Closure",
     "headline": "Aklan Mayor ready to face probe over Boracay issues.",
-    "imageUrl": "https://example.com/images/boracay-2018.jpg",
+    "imageUrl": "/dg-headlines/2018.jpg",
     "link": "https://www.facebook.com/share/p/18ZGTRswXX/"
   },
   {
     "date": "August 9, 2019",
     "topic": "Guimaras Tragedy",
     "headline": "OVERHAUL LOOMS after Iloilo Strait tragedy.",
-    "imageUrl": "https://example.com/images/guimaras-2019.jpg"
+    "imageUrl": "/dg-headlines/2019.jpg"
   },
   {
     "date": "July 20, 2020",
     "topic": "COVID-19",
     "headline": "More than 70% of rural health units infected with COVID-19.",
-    "imageUrl": "https://example.com/images/covid-2020.jpg",
+    "imageUrl": "/dg-headlines/2020.jpg",
     "link": "https://www.facebook.com/share/p/1CMRiQQ4wj/"
   },
   {
     "date": "September 1, 2021",
     "topic": "COVID Vaccine",
     "headline": "5.6% of COVID cases were fully vaccinated.",
-    "imageUrl": "https://example.com/images/vaccine-2021.jpg",
+    "imageUrl": "/dg-headlines/2021.jpg",
     "link": "https://www.facebook.com/share/p/18UwifmkkU/"
   },
   {
     "date": "March 2, 2022",
     "topic": "Face-to-Face Classes",
     "headline": "Limited face-to-face classes start in Iloilo.",
-    "imageUrl": "https://example.com/images/classes-2022.jpg",
+    "imageUrl": "/dg-headlines/2022.jpg",
     "link": "https://www.facebook.com/share/p/17tzKw5WUi/"
   },
   {
     "date": "October 1, 2023",
     "topic": "Confidential Funds",
     "headline": "COA: Davao City tops confidential funds spending.",
-    "imageUrl": "https://example.com/images/confidential-funds-2023.jpg",
+    "imageUrl": "/dg-headlines/2023.jpg",
     "link": "https://www.facebook.com/share/p/1H9f1p4pLw/"
   },
   {
     "date": "March 9, 2024",
     "topic": "WVSUCAT",
     "headline": "20,925 students to take WVSUCAT.",
-    "imageUrl": "https://example.com/images/wvsucat-2024.jpg",
+    "imageUrl": "/dg-headlines/2024.jpg",
     "link": "https://www.facebook.com/share/p/14d7QKZaorK/"
   },
   {
     "date": "March 12, 2025",
     "topic": "Duterte Arrested",
     "headline": "‘HE HAD IT COMING’: Ex-Pres Duterte arrested for crimes against humanity.",
-    "imageUrl": "https://example.com/images/duterte-2025.jpg",
+    "imageUrl": "/dg-headlines/2025.jpg",
     "link": "https://dailygcanauardian.com.ph/he-had-it-coming-ex-pres-duterte-arrested-for-crimes-against-humanity/"
   }
 ];
